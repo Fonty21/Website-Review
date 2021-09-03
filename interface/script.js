b03243381d9df9cf415e2823518f2cf2679d868e
Jacob FONTANAROSA <jacob.fontanarosa@students.emmauscc.sa.edu.au>
2021-09-03 14:43:48 +0930

    Created reset button and added submit button

---
+++ interface/script.js
@@ -0,0 +1,10 @@
+function reset(){
+    $("#placeholderFirst").val("").attr("placeholder", "First Name");
+    $("#placeholderLast").val("").attr("placeholder", "Last Name");
+    $("#DropDown").val("").attr("selected", "Star Rating");
+    $("#reviewBox").val("").attr("placeholder", "Enter Review");
+}
+
+function submit(){
+    
+}
