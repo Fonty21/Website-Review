06af514d64971888dbacb412dba8af0c7276d4d5
Jacob FONTANAROSA <jacob.fontanarosa@students.emmauscc.sa.edu.au>
2021-09-07 11:46:06 +0930

    Started function to search box

--- interface/script.js
+++ interface/script.js
@@ -1,4 +1,9 @@
+function search(){
+    $("searchBar")
+}
+
 function reset(){
+    $("#searchBar").val("").attr("placeholder", "Search Game");
     $("#placeholderFirst").val("").attr("placeholder", "First Name");
     $("#placeholderLast").val("").attr("placeholder", "Last Name");
     $("#DropDown").val("").attr("selected", "Star Rating");
